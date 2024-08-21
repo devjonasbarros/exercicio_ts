163d6d5dff72283b8e451aec2a62cedf88bd42ac
jonas Barros <jangelocc@gmail.com>
2024-08-21 06:21:00 -0300

    exercicio ts

---
+++ src/exercicio.ts
@@ -0,0 +1,31 @@
+// Função para multiplicar dois valores
+
+// modo um de multiplicação
+function multiplica(valorA:number, valorB:number): number{
+    return valorA * valorB;
+}
+
+
+// Segunda forma de multiplicação de dois valores
+const mult = (valorA:number, valorB:number): number => valorA*valorB;
+
+// Funçoes no typescript
+function nome2 (seuNome: string){
+    return 'Ola' + seuNome;
+}
+
+
+
+// const saudacao = [
+//     {
+//     nome: "jonas"
+// }
+
+// ]
+
+// let nome4: string = 'jonas';
+// console.log(nome4)
+
+// function mostrar (nome3: nome){
+//     console.log(mostrar.nome);
+// }
---
+++ src/tipos_basicos.ts
@@ -0,0 +1,14 @@
+// Variaveis no typescript
+
+let estaChovendo: boolean = false
+estaChovendo = true
+
+let idade: number = 27
+let altura: number = 1.75
+const nacionalidade: string = 'brasileira'
+
+const colegas: string[] = ['lucas','bianca']
+const tecnologias: Array<string> = ['html', 'css', 'js']
+const notas: ReadonlyArray<Number> = [7,9,5,8]
+const lista: [nome: string, estaEstudando: boolean, idade: number] = ['jonas', true, 27]
+
